6173918567a8cfc7a7e20eeda0f3699326364d6d
Randy Palamar <palamar@ualberta.ca>
2023-10-10 20:55:56 -0600

    vis_pipe: correctly return non-zero exit status
    according to POSIX wait(3p) the return status needs to be checked
    and the macro WEXITSTATUS(stat_val) should be used to get the actual
    return value on a normal exit. POSIX doesn't specify the value of
    stat_val on abnormal exit and thus vis_pipe() should just return
    -1 as it does for other errors
    
    closes #1130: vis:pipe returns wrong exit status (when non-zero)
    
    Thanks @pippi1otta for the report and suggestion.

--- vis.c
+++ vis.c
@@ -1930,7 +1930,10 @@ err:
 	vis->interrupted = false;
 	vis->ui->terminal_restore(vis->ui);
 
-	return status;
+	if (WIFEXITED(status))
+		return WEXITSTATUS(status);
+
+	return -1;
 }
 
 static ssize_t read_buffer(void *context, char *data, size_t len) {
